84f73eca90b755b07d5d7992d1683e8c89179abf
Lina Franco <linafranco31@email.com>
2023-08-29 00:26:37 -0500

    Ajuste tipo de letra

--- src/components/homeComponents/characterDetail/modal/ModalComponent.tsx
+++ src/components/homeComponents/characterDetail/modal/ModalComponent.tsx
@@ -19,7 +19,7 @@ export const ModalComponent = ({ setShowModal, showModal, comment, setComment, h
                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                                 {/*header*/}
                                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                                    <h3 className="text-3xl font-semibold">
+                                    <h3 className="text-3xl font-semibold font-greycliff">
                                         Add a comment to the character
                                     </h3>
                                     <button
